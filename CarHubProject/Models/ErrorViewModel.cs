f313d559a397509dc5f9543dfd5610b4bb675651
Abdelrahman Saber <dadsaber233@gmail.com>
2025-10-31 13:15:30 +0200

    Cleaned up .vs and added .gitignore

---
+++ CarHubProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarHubProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
